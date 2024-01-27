8f71163951fc78619c3f6b5dc1e4a36a19a49a47
Aalaa Habib <aalaahabib.ah@gmail.com>
2024-01-27 14:08:36 +0200

    add comments and enhance the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,87 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './services/app.service';
+import { IImage } from './models/image.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'drag-drop-images';
-  leftBoxImages: { url: string; number: number; checked: boolean }[] = [
-    {
-      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-      number: 1,
-      checked: false,
-    },
-    {
-      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-      number: 2,
-      checked: false,
-    },
-    {
-      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-      number: 3,
-      checked: false,
-    },
-    {
-      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-      number: 4,
-      checked: false,
-    },
-    {
-      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-      number: 5,
-      checked: false,
-    },
-    {
-      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-      number: 6,
-      checked: false,
-    },
-    {
-      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-      number: 7,
-      checked: false,
-    },
-    {
-      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-      number: 8,
-      checked: false,
-    },
-  ];
+  constructor(private _appService: AppService) {}
 
-  rightBoxImages: { url: string; number: number }[] = [
-    {
-      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-      number: 9,
-    },
-    {
-      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-      number: 10,
-    },
-    {
-      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-      number: 11,
-    },
-  ];
+  ngOnInit(): void {
+    this.leftBoxImages = this._appService.leftBoxImages;
+    this.rightBoxImages = this._appService.rightBoxImages;
+  }
+  
+  // Array of images in the left box
+  leftBoxImages: IImage[] = [];
+  // Array of images in the right box
+  rightBoxImages: IImage[] = [];
 
+  // Event handler for drag start
   onDragStart(
     event: DragEvent,
     image: { url: string; number: number; checked?: boolean }
   ) {
-    image.checked = true;
-    event.dataTransfer?.setData('text/plain', image.url);
-    event.dataTransfer?.setData('text/number', image.number.toString());
+    //the user can't drag-drop the image till checking the input box
+    if (image.checked) {
+      // Set image data in the dataTransfer object
+      event.dataTransfer?.setData('text/plain', image.url);
+      event.dataTransfer?.setData('text/number', image.number.toString());
+    }
   }
 
+  // Event handler for drop
   onDrop(event: DragEvent, box: string) {
     const imageUrl = event.dataTransfer?.getData('text/plain');
     const number = event.dataTransfer?.getData('text/number');
+    // Check if the required data is present and the drop target is the right box
     if (imageUrl && number && box === 'right') {
       const parsedNumber = parseInt(number);
+      // Find the image in the left box based on URL and number
       const image = this.leftBoxImages.find(
         (img) => img.url === imageUrl && img.number === parsedNumber
       );
+
+      // If the image is found, add it to the right box
       if (image) {
         this.rightBoxImages.push({ url: image.url, number: image.number });
         image.checked = false;
@@ -90,18 +55,17 @@ export class AppComponent {
     }
   }
 
+  // Event handler to allow dropping
   allowDrop(event: DragEvent) {
     event.preventDefault();
   }
 
-  removeImageFromLeftBox(image: {
-    url: string;
-    number: number;
-    checked: boolean;
-  }) {
+  // Remove image from the left box
+  removeImageFromLeftBox(image: IImage) {
     const index = this.leftBoxImages.findIndex(
       (img) => img.url === image.url && img.number === image.number
     );
+    // If the image is found, remove it from the array
     if (index !== -1) {
       this.leftBoxImages.splice(index, 1);
     }

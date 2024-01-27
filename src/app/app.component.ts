a4420c3ab798fc463486d6b4c09ce7aac55be578
Aalaa Habib <aalaahabib.ah@gmail.com>
2024-01-27 07:13:51 +0200

    add checkbox and format code'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,71 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'drag-drop-images';
-  leftBoxImages: { url: string; number: number }[] = [
-    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 1 },
-    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 2 },
-    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 3 },
-    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 4 },
-    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 5 },
-    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 6 },
-    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 7 },
-    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 8 },
+  leftBoxImages: { url: string; number: number; checked: boolean }[] = [
+    {
+      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
+      number: 1,
+      checked: false,
+    },
+    {
+      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
+      number: 2,
+      checked: false,
+    },
+    {
+      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
+      number: 3,
+      checked: false,
+    },
+    {
+      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
+      number: 4,
+      checked: false,
+    },
+    {
+      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
+      number: 5,
+      checked: false,
+    },
+    {
+      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
+      number: 6,
+      checked: false,
+    },
+    {
+      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
+      number: 7,
+      checked: false,
+    },
+    {
+      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
+      number: 8,
+      checked: false,
+    },
   ];
 
-
-  rightBoxImages:  { url: string; number: number }[] = [
-    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 9 },
-    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 10 },
-    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 11 },
+  rightBoxImages: { url: string; number: number }[] = [
+    {
+      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
+      number: 9,
+    },
+    {
+      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
+      number: 10,
+    },
+    {
+      url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
+      number: 11,
+    },
   ];
 
-  onDragStart(event: DragEvent, imageUrl: string, number: number) {
-    event.dataTransfer?.setData('text/plain', imageUrl);
-    event.dataTransfer?.setData('text/number', number.toString());
+  onDragStart(
+    event: DragEvent,
+    image: { url: string; number: number; checked?: boolean }
+  ) {
+    image.checked = true;
+    event.dataTransfer?.setData('text/plain', image.url);
+    event.dataTransfer?.setData('text/number', image.number.toString());
   }
 
   onDrop(event: DragEvent, box: string) {
@@ -35,8 +79,14 @@ export class AppComponent {
     const number = event.dataTransfer?.getData('text/number');
     if (imageUrl && number && box === 'right') {
       const parsedNumber = parseInt(number);
-      this.rightBoxImages.push({ url: imageUrl, number: parsedNumber });
-      this.removeImageFromLeftBox(imageUrl,parsedNumber);
+      const image = this.leftBoxImages.find(
+        (img) => img.url === imageUrl && img.number === parsedNumber
+      );
+      if (image) {
+        this.rightBoxImages.push({ url: image.url, number: image.number });
+        image.checked = false;
+        this.removeImageFromLeftBox(image);
+      }
     }
   }
 
@@ -44,8 +94,14 @@ export class AppComponent {
     event.preventDefault();
   }
 
-  removeImageFromLeftBox(imageUrl: string, number: number) {
-    const index = this.leftBoxImages.findIndex(image => image.url === imageUrl && image.number === number);
+  removeImageFromLeftBox(image: {
+    url: string;
+    number: number;
+    checked: boolean;
+  }) {
+    const index = this.leftBoxImages.findIndex(
+      (img) => img.url === image.url && img.number === image.number
+    );
     if (index !== -1) {
       this.leftBoxImages.splice(index, 1);
     }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {MatIconModule} from '@angular/material/icon';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    FormsModule      
   ],
   providers: [],
   bootstrap: [AppComponent]

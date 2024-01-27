e5c6e3948bb5da182e0b33e3a4b4334da5dda893
Aalaa Habib <aalaahabib.ah@gmail.com>
2024-01-27 06:11:08 +0200

    update template design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,42 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'drag-drop-images';
-  leftBoxImages: string[] = [
-    'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-    'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-    'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-    'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-    'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-    'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-    'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-    'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
+  leftBoxImages: { url: string; number: number }[] = [
+    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 1 },
+    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 2 },
+    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 3 },
+    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 4 },
+    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 5 },
+    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 6 },
+    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 7 },
+    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 8 },
   ];
 
-  rightBoxImages: string[] = [
-    'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-    'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
-    'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png',
+
+  rightBoxImages:  { url: string; number: number }[] = [
+    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 9 },
+    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 10 },
+    { url: 'https://privacyterms.io/wp-content/uploads/terms-of-service-preview.png', number: 11 },
   ];
 
-  onDragStart(event: DragEvent, imageUrl: string) {
+  onDragStart(event: DragEvent, imageUrl: string, number: number) {
     event.dataTransfer?.setData('text/plain', imageUrl);
+    event.dataTransfer?.setData('text/number', number.toString());
   }
 
   onDrop(event: DragEvent, box: string) {
     const imageUrl = event.dataTransfer?.getData('text/plain');
-    if (imageUrl && box === 'right') {
-      this.rightBoxImages.push(imageUrl);
-      this.removeImageFromLeftBox(imageUrl);
-    }
-  }
-  removeImageFromLeftBox(imageUrl: string) {
-    const index = this.leftBoxImages.indexOf(imageUrl);
-    if (index !== -1) {
-      this.leftBoxImages.splice(index, 1);
+    const number = event.dataTransfer?.getData('text/number');
+    if (imageUrl && number && box === 'right') {
+      const parsedNumber = parseInt(number);
+      this.rightBoxImages.push({ url: imageUrl, number: parsedNumber });
+      this.removeImageFromLeftBox(imageUrl,parsedNumber);
     }
   }
 
   allowDrop(event: DragEvent) {
     event.preventDefault();
+  }
+
+  removeImageFromLeftBox(imageUrl: string, number: number) {
+    const index = this.leftBoxImages.findIndex(image => image.url === imageUrl && image.number === number);
+    if (index !== -1) {
+      this.leftBoxImages.splice(index, 1);
+    }
   }
 }
